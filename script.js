a6420b25e12ca4b3d527b8a20d9b899559c54b8e
shuntarouranishi <shuntaro.uranishi@gmail.com>
2023-11-27 20:01:51 +0900

    クイズ

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let timeLeft = 20; // 60秒
+let timeLeft = 10; // 30秒の制限時間
 const timerElement = document.getElementById("timer");
+const resultElement = document.getElementById("result");
 const paymentLinkElement = document.getElementById("paymentLink");
+const correctAnswer = "apple";
 
 const interval = setInterval(() => {
   timeLeft--;
@@ -8,6 +10,21 @@ const interval = setInterval(() => {
 
   if (timeLeft <= 0) {
     clearInterval(interval);
-    paymentLinkElement.style.display = "block"; // PayPayリンクを表示
+    showPaymentLink();
   }
 }, 1000);
+
+function submitAnswer() {
+  clearInterval(interval);
+  const userAnswer = document.getElementById("answer").value;
+  if (userAnswer === correctAnswer) {
+    resultElement.textContent = "Correct!";
+  } else {
+    showPaymentLink();
+  }
+}
+
+function showPaymentLink() {
+  resultElement.textContent = "Time's up or wrong answer!";
+  paymentLinkElement.style.display = "block";
+}
